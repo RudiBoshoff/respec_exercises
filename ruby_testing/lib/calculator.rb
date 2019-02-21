ac7720e6c88367f3eab6d3d91edb70c56f2bfb0e
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-21 14:00:34 +0200

    Features: changes formatting of test results. Docs: improves calculator readability

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -1,17 +1,17 @@
 class Calculator
-  def add(a,b,c = 0)
+  def add(a, b, c = 0)
     a + b + c
   end
 
-  def multiply(a,b)
+  def multiply(a, b)
     a * b
   end
 
-  def subtract(a,b)
+  def subtract(a, b)
     a - b
   end
 
-  def divide(a,b)
+  def divide(a, b)
     a.to_f / b.to_f
   end
 end
